6da7804d1c6f9d6ab8e834dcfab77c9dee28760b
Giuseppe <giuggey85@gmail.com>
2023-12-12 11:03:09 +0000

    add section

--- scripts/api.js
+++ scripts/api.js
@@ -8,6 +8,13 @@ const requestApi = async () => {
   const response = await axios.get(`${url}${people}`);
   const data = response.data.results;
   console.log(data);
+
+  let cardContainerEl = document.querySelector('.card__container');
+  for (let i = 0; i < data.length; i++) {
+    let cardEl = document.createElement('article');
+    cardEl.classList.add('card');
+    cardContainerEl.appendChild(cardEl);
+  }
 };
 
 requestApi();
