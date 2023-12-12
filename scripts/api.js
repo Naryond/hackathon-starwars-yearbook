63aecff8dce9723c62904b6d5c375dfa547cf70c
Calvin <calvinng1993@gmail.com>
2023-12-12 15:41:02 +0000

    Add other pages onto page

--- scripts/api.js
+++ scripts/api.js
@@ -1,8 +1,8 @@
 // Base url
-const url = 'https://swapi.dev/api/';
+const url = "https://swapi.dev/api/";
 
 // Delving into people section
-const people = 'people/';
+const people = "people/";
 
 class StarWarsApi {
   constructor(url, people) {
@@ -14,58 +14,70 @@ class StarWarsApi {
     // GET request and stored in the data const
     const response = await axios.get(`${this.url}${this.people}`);
     const data = response.data.results;
+    getData(data);
+  };
 
-
-
-    let cardContainerEl = document.querySelector('.card__container');
-
-
-    for (let i = 0; i < data.length; i++) {
-      // Creating an article for every Character, applying a class to it, append it
-      let cardEl = document.createElement('article');
-      cardEl.classList.add('card');
-      cardContainerEl.appendChild(cardEl);
-
-      let nameEl = document.createElement('h3');
-      nameEl.classList.add('card__name');
-      nameEl.innerHTML = data[i].name;
-      cardEl.appendChild(nameEl);
-
-      // Subcontainers inside the article
-      let subcontainerEl = document.createElement('div');
-      subcontainerEl.classList.add('card__subcontainer');
-      cardEl.appendChild(subcontainerEl);
-
-      let logoSideEL = document.createElement('div');
-      logoSideEL.classList.add('card__logo');
-      subcontainerEl.appendChild(logoSideEL);
-
-      let descriptionSideEL = document.createElement('div');
-      descriptionSideEL.classList.add('card__description');
-      subcontainerEl.appendChild(descriptionSideEL);
-
-      // Subcontainers inside the logo
-      let photoEl = document.createElement('img');
-      photoEl.classList.add('card__logo-image');
-      photoEl.setAttribute('src', `../assets/images/people/${i + 1}.jpg`);
-      logoSideEL.append(photoEl);
-
-      // Dividing the description block
-
-      let genderEl = document.createElement('p');
-      genderEl.classList.add('card__description-gender');
-      genderEl.innerHTML = `Gender: ${data[i].gender}`;
-      descriptionSideEL.append(genderEl);
-
-      let birthdayEl = document.createElement('p');
-      birthdayEl.classList.add('card__description-birth');
-      birthdayEl.innerHTML = `Year of Birth: ${data[i].birth_year}`;
-      descriptionSideEL.append(birthdayEl);
+  getNextPage = async () => {
+    for (let i = 1; i < 10; i++) {
+      const response = await axios.get(`${this.url}${this.people}?page=${i}`);
+      const data = response.data.results;
+      console.log(data);
+      getData(data, i);
     }
   };
 }
 
+function getData(data, pageNum) {
+  let cardContainerEl = document.querySelector(".card__container");
+
+  for (let i = 0; i < data.length; i++) {
+    // Creating an article for every Character, applying a class to it, append it
+    let cardEl = document.createElement("article");
+    cardEl.classList.add("card");
+    cardContainerEl.appendChild(cardEl);
+
+    let nameEl = document.createElement("h3");
+    nameEl.classList.add("card__name");
+    nameEl.innerHTML = data[i].name;
+    cardEl.appendChild(nameEl);
+
+    // Subcontainers inside the article
+    let subcontainerEl = document.createElement("div");
+    subcontainerEl.classList.add("card__subcontainer");
+    cardEl.appendChild(subcontainerEl);
+
+    let logoSideEL = document.createElement("div");
+    logoSideEL.classList.add("card__logo");
+    subcontainerEl.appendChild(logoSideEL);
+
+    let descriptionSideEL = document.createElement("div");
+    descriptionSideEL.classList.add("card__description");
+    subcontainerEl.appendChild(descriptionSideEL);
+
+    // Subcontainers inside the logo
+    let photoEl = document.createElement("img");
+    photoEl.classList.add("card__logo-image");
+    photoEl.setAttribute(
+      "src",
+      `../assets/images/people/${(pageNum - 1) * 10 + 1 + i}.jpg`
+    );
+    logoSideEL.append(photoEl);
+
+    // Dividing the description block
+
+    let genderEl = document.createElement("p");
+    genderEl.classList.add("card__description-gender");
+    genderEl.innerHTML = `Gender: ${data[i].gender}`;
+    descriptionSideEL.append(genderEl);
+
+    let birthdayEl = document.createElement("p");
+    birthdayEl.classList.add("card__description-birth");
+    birthdayEl.innerHTML = `Year of Birth: ${data[i].birth_year}`;
+    descriptionSideEL.append(birthdayEl);
+  }
+}
+
 const starWars = new StarWarsApi(url, people);
 
-starWars.requestApi();
-
+// starWars.requestApi();
+starWars.getNextPage(1);
